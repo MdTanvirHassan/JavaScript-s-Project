02c31aec63f979913fd096e0b5c7c19c000def9f
Tanvir Hassan <103600961+MdTanvirHassan@users.noreply.github.com>
2022-10-13 23:45:20 +0600

    Basic JS Project
    Update Pioneer Bank

---
+++ PioneerBank/index.js
@@ -0,0 +1,72 @@
+//login event handeler
+const trArea= document.getElementById("transaction-area").style.display= "none";
+const loginBtn = document.getElementById("login-btn");
+loginBtn.addEventListener("click",function(){
+    const loginArea = document.getElementById("login-area");
+loginArea.style.display= "none";
+const transactionArea = document.getElementById("transaction-area");
+transactionArea.style.display= "block";
+transactionArea.style.backgroundColor= "";
+
+function updateSpanText(id,addNumber) {
+            const currentAmount= document.getElementById(id).innerText;
+           const currentNumber = parseFloat(currentAmount); 
+           const totalAmount = addNumber + currentNumber;
+           document.getElementById(id).innerText = totalAmount;
+           
+        }
+
+//Deposit event handler
+    const depositBtn = document.getElementById("addDeposit");
+     depositBtn.addEventListener("click",function(){
+        const depositAmount= document.getElementById("depositAmount").value;
+        const depositNumber = parseFloat(depositAmount);
+        updateSpanText("currentDeposit",depositNumber );
+        updateSpanText("currentBalance",depositNumber );
+
+        document.getElementById("depositAmount").value="";
+
+       
+        /*const currentDeposit = document.getElementById("currentDeposit").innerText;
+        const currentDepositNumber = parseFloat(currentDeposit); 
+        const totalDeposit = depositNumber + currentDepositNumber;
+        document.getElementById("currentDeposit").innerText = totalDeposit;
+        document.getElementById("depositAmount").value="";
+
+        //TotalBalance
+        const currentBalance= document.getElementById("currentBalance").innerText;
+        const currentBalanceNumber = parseFloat(currentBalance);
+        const TotalBalance= currentBalanceNumber + totalDeposit;
+        document.getElementById("currentBalance").innerText=TotalBalance;*/
+
+
+    });
+
+
+    //Withdraw event handler
+
+    const WithdrawBtn = document.getElementById("addWithdraw");
+    WithdrawBtn.addEventListener("click",function(){
+        const withdrawAmount= document.getElementById("withdrawAmount").value;
+        const withdrawAmountNumber = parseFloat(withdrawAmount);
+
+        updateSpanText("currentWithdraw",withdrawAmountNumber);
+        updateSpanText("currentBalance",-1*withdrawAmountNumber);
+        document.getElementById("withdrawAmount").value="";
+        /*const currentWithdraw= document.getElementById("currentWithdraw").innerText;
+        const currentWithdrawNumber = parseFloat(currentWithdraw);
+        const totalWithdraw = withdrawAmountNumber + currentWithdrawNumber;
+        document.getElementById("currentWithdraw").innerText= totalWithdraw;
+        document.getElementById("withdrawAmount").value="";*
+
+        //TotalBalance
+     const currentBalance= document.getElementById("currentBalance").innerText;
+        const currentBalanceNumber = parseFloat(currentBalance);
+        const TotalBalance= currentBalanceNumber - totalWithdraw;
+        document.getElementById("currentBalance").innerText=TotalBalance;*/
+        
+    });
+
+   
+
+});

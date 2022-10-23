ac851066eca472a7e5be9bf9dd8ecbfa71c01682
Tanvir Hassan <103600961+MdTanvirHassan@users.noreply.github.com>
2022-10-24 00:03:32 +0600

    Open Weather Map API

---
+++ Temperature-hot/index.js
@@ -0,0 +1,26 @@
+
+const API_KEY = `46ad7457603b9b0104e633e78cd60e16`;
+const searchTemperature = () => {
+    const city = document.getElementById('city-name').value;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
+
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayTemperature(data));
+}
+
+const setInnerText = (id, text) => {
+    document.getElementById(id).innerText = text;
+}
+
+const displayTemperature = temperature => {
+    setInnerText('city', temperature.name='Dhaka');
+    setInnerText('temperature', temperature.main.temp);
+    setInnerText('condition', temperature.weather[0].main);
+    // set weather icon
+    const url = `http://openweathermap.org/img/wn/${temperature.weather[0].icon}@2x.png`;
+    const imgIcon = document.getElementById('weather-icon');
+    imgIcon.setAttribute('src', url);
+}
+
+//setInterval(()=> {data.API_KEY},200);

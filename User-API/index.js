069c3e3f94311c5f5ff26eaf0b0ba3471450942d
Tanvir Hassan <103600961+MdTanvirHassan@users.noreply.github.com>
2022-10-27 18:51:30 +0600

    Updating search API & style

--- User-API/index.js
+++ User-API/index.js
@@ -1,34 +1,42 @@
-const API= `https://jsonplaceholder.typicode.com/users`;
-fetch(API)
-.then(res => res.json())
-.then(data => {
-    const userContainer = document.getElementById('user-container');
-    userContainer.style.color= "royalBlue";
-   
+const inputBtn= document.getElementById('input-btn');
+inputBtn.addEventListener('click',function(){
+    const userInput= document.getElementById('user-input').value;
+    const idCount= parseInt(userInput);
 
-    for (let i = 0; i < data.length; i++) {
-        const user = data[i];
-        const p = document.createElement('p');
-        p.innerHTML = `<p>
-        Name: <strong>${user.name}   </strong>
-        <button onclick="getUserDetails(${user.id})">
-       details ${user.id}
-        </button></p>`;
-        userContainer.appendChild(p);
-    }
-});
+    const API= `https://jsonplaceholder.typicode.com/users`;
+    fetch(API)
+    .then(res => res.json())
+    .then(data => {
+        data=data.slice(0,idCount);
+         const userContainer = document.getElementById('user-container');
+         userContainer.innerText='';
+        userContainer.style.color= "royalBlue";
+    
+
+        for (let i = 0; i < data.length; i++) {
+            const user = data[i];
+            const p = document.createElement('p');
+            p.innerHTML = `<span><strong>
+            Name: ${user.name}   </strong>
+            <button onclick="getUserDetails(${user.id})">
+        details ${user.id}
+            </button></span>`;
+            userContainer.appendChild(p);
+        }
+    });
+})
+
 
 function getUserDetails(userId) {
     fetch(`https://jsonplaceholder.typicode.com/users/${userId}`)
 .then(res => res.json())
 .then(data => {
     const details = document.getElementById('user-details');
-    details.style.background="red";
-    details.style.boxShadow= "5px 5px 15px solid gray";
+    details.style.color="red"; 
     details.innerHTML =`
-        <h3>${data.name}</h3>
-        <h4>${data.email}</h4>
-        <h5>${data.phone}</h5>
+        <h4>${data.name}</h4>
+        <h5>${data.email}</h5>
+        <h6>${data.phone}</h6>
  `
 })
 };
